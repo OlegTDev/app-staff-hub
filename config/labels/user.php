7ab0fd9084ca7a84cbd0ef0d9eef561dca21a725
OlegTDev <_alexeevich_@list.ru>
2026-08-20 16:36:47 +0500

    feat: labels для User

---
+++ config/labels/user.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'id' => '#',
+    'name' => 'Имя',
+    'email' => 'Почта',
+    'password' => 'Пароль',
+    'login' => 'Логин',
+    'company' => 'Организация',
+    'department' => 'Отдел',
+    'position' => 'Должность',
+    'telephone' => 'Телефон',
+    'domain' => 'Домен',
+    'created_at' => 'Дата создания',
+    'updated_at'=> 'Дата изменения',
+];
